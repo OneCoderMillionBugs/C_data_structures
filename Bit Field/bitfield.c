008694b6991ecde52245d40a0d1e674b82b8372a
Ilyas <111341551+OneCoderMillionBugs@users.noreply.github.com>
2022-09-07 16:48:08 +0600

    Add files via upload

---
+++ Bit Field/bitfield.c
@@ -0,0 +1,55 @@
+#include "bitfield.h"
+
+// Since information is contained in 64-bit integers
+// The real size of arr will be N, where N is smallest number that is
+// equal or bigger than required size, and divisible by 8
+Bitfield *bitfield_create(size_t size)
+{
+    Bitfield *bf = (Bitfield *)malloc(sizeof(Bitfield));
+    bf->size = size;
+    bf->occm = (size % 8) ? size / 8 + 1 : size / 8;
+    bf->arr = (uint8_t *)calloc(0, bf->occm);
+    return bf;
+}
+
+// Set bitfield value at some index
+void bitfield_set(size_t index, _Bool value, Bitfield *bf)
+{
+    if (index >= bf->size)
+    {
+        ERROR("%s:%d: error: Index out of bounds\n");
+    }
+
+    if (value)
+    {
+        bf->arr[index / 8] |= (1 << 7 - index % 8);
+    }
+    else
+    {
+        bf->arr[index / 8] &= ~(1 << 7 - index % 8);
+    }
+}
+
+void bitfield_clear(Bitfield *bf)
+{
+    memset(bf->arr, 0, bf->occm);
+}
+
+uint8_t bitfield_get(size_t index, Bitfield *bf)
+{
+    if (index >= bf->size)
+    {
+        ERROR("%s:%d: error: Index out of bounds\n");
+    }
+
+    return bf->arr[index / 8] >> (7 - index % 8) & 1;
+}
+
+void bitfield_print(Bitfield *bf)
+{
+    for (size_t i = 0; i < bf->size; i++)
+    {
+        printf("%hhu ", bitfield_get(i, bf));
+    }
+    puts("");
+}
---
+++ Bit Field/bitfield.h
@@ -0,0 +1,26 @@
+#ifndef BITFIELD_H
+#define BITFIELD_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ERROR(msg) \
+    (fprintf(stderr, msg, __FILE__, __LINE__), exit(-1))
+
+struct bitfield
+{
+    size_t size;    // Size entered by user
+    size_t occm;    // Occupied memory size in bytes
+    uint8_t *arr;   // The bitfield itself
+}
+typedef Bitfield;
+
+Bitfield *bitfield_create(size_t size);
+void bitfield_set(size_t index, _Bool value, Bitfield *bf);
+void bitfield_clear(Bitfield *bf);
+uint8_t bitfield_get(size_t index, Bitfield *bf);
+void bitfield_print(Bitfield *bf);
+
+#endif
