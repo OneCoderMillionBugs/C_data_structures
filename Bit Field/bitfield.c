04e898be982d6e7cc010b64dc1d28c15c6b66380
Ilyas <111341551+OneCoderMillionBugs@users.noreply.github.com>
2023-07-18 16:02:07 +0600

    Add files via upload

--- Bit Field/bitfield.c
+++ Bit Field/bitfield.c
@@ -11,6 +11,11 @@ Bitfield *bitfield_create(size_t size)
 
 void bitfield_set(size_t index, _Bool value, Bitfield *bf)
 {
+    if (index >= bf->size)
+    {
+        ERROR("%s:%d: error: Index out of bounds\n");
+    }
+
     if (value)
     {
         bf->arr[index / 8] |= (1 << 7 - index % 8);
@@ -28,5 +33,10 @@ void bitfield_clear(Bitfield *bf)
 
 uint8_t bitfield_get(size_t index, Bitfield *bf)
 {
+    if (index >= bf->size)
+    {
+        ERROR("%s:%d: error: Index out of bounds\n");
+    }
+
     return bf->arr[index / 8] >> (7 - index % 8) & 1;
 }
